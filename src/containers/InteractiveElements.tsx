ff20e22dd9e3f69438e22e3ff49fc921999c480f
Joanna Chądzyńska <jmalek87@gmail.com>
2021-11-17 21:40:39 +0100

    finish design system component

--- src/containers/InteractiveElements.tsx
+++ src/containers/InteractiveElements.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/role-supports-aria-props */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/anchor-has-content */
 import { ExploreButton } from "../components";
@@ -44,16 +45,64 @@ const InteractiveElements: React.FC<InteractiveElementsProps> = () => {
             </div>
 
             <div className='flex'>
-                <div style={{ marginTop: "5rem" }} className='flow'>
+                <div style={{ marginTop: "5rem" }}>
                     {/* explore button */}
                     <ExploreButton />
                     <p>Landing Page Main Button</p>
                 </div>
 
-                <div className='flow' style={{ marginBottom: "50vh" }}>
+                <div style={{ marginBottom: "50vh" }} className='flow'>
                     {/* tabs */}
+                    <div className='tab-list underline-indicators flex'>
+                        <button
+                            aria-selected='true'
+                            className='uppercase text-accent letter-spacing-2 bg-dark ff-sans-cond'>
+                            Moon
+                        </button>
+                        <button
+                            aria-selected='false'
+                            className='uppercase text-accent letter-spacing-2 bg-dark ff-sans-cond'>
+                            Mars
+                        </button>
+                        <button
+                            aria-selected='false'
+                            className='uppercase text-accent letter-spacing-2 bg-dark ff-sans-cond'>
+                            Europa
+                        </button>
+                    </div>
+
                     {/* dots */}
+                    <div className='dot-indicators flex'>
+                        <button aria-selected='true'>
+                            <span className='sr-only'>Slide title</span>
+                        </button>
+                        <button aria-selected='false'>
+                            <span className='sr-only'>Slide title</span>
+                        </button>
+                        <button aria-selected='false'>
+                            <span className='sr-only'>Slide title</span>
+                        </button>
+                    </div>
+
                     {/* numbers */}
+
+                    <div
+                        className='number-indicators flex fs-600'
+                        style={{
+                            flexDirection: "column",
+                            justifyContent: "center",
+                            alignItems: "center",
+                        }}>
+                        <button aria-selected='true'>
+                            <span>1</span>
+                        </button>
+                        <button aria-selected='false'>
+                            <span>2</span>
+                        </button>
+                        <button aria-selected='false'>
+                            <span>3</span>
+                        </button>
+                    </div>
                 </div>
             </div>
         </section>

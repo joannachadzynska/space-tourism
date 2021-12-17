d95e77c53640e27c5ccb5ba82062fe484dca2073
Joanna Chądzyńska <jmalek87@gmail.com>
2021-12-17 20:34:45 +0100

    zxc

--- apps/space-tourism/src/models/crew.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Crew {
-    name: string;
-    images: {
-        png: string;
-        webp: string;
-    };
-    role: string;
-    bio: string;
-}
--- apps/space-tourism/src/routing/Routes.ts
+++
@@ -1,6 +0,0 @@
-export const ROUTES = {
-    HOME: "/",
-    DESTINATION: "destinations",
-    CREW: "crew",
-    TECHNOLOGY: "technology",
-};

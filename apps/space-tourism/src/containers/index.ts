afbc59d38c8e6e5dff779c2d6b8152fee1af3034
Joanna Chądzyńska <jmalek87@gmail.com>
2021-12-17 19:24:34 +0100

    fixes

--- apps/space-tourism/src/containers/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as DesignSystem } from "./DesignSystem";
-export { default as Header } from "./Header";
-export { default as Layout } from "./Layout";
--- apps/space-tourism/src/pages/Technology.tsx
+++
@@ -1,7 +0,0 @@
-interface TechnologyPageProps {}
-
-const TechnologyPage: React.FunctionComponent<TechnologyPageProps> = () => {
-    return <section>Technology</section>;
-};
-
-export default TechnologyPage;
